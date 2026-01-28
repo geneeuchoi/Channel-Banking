d02f958ca9523472cc08687240cd7c1431e25793
geneeuchoi <chyj2108@naver.com>
2026-01-28 19:24:54 +0900

    config

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS transaction_histories;
+
+CREATE TABLE users (
+                       user_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                       email VARCHAR(100) NOT NULL UNIQUE,
+                       name VARCHAR(50) NOT NULL,
+                       password VARCHAR(255) NOT NULL,
+                       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                       updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE transaction_histories (
+                                       transaction_history_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                                       sender_user_id BIGINT NOT NULL,
+                                       receiver_user_id BIGINT,
+                                       amount BIGINT,
+                                       sender_balance_after BIGINT,
+                                       receiver_balance_after BIGINT,
+                                       type VARCHAR(50) NOT NULL
+);
