b2486d9bc2bab3edfc667337b133b6dcee4606d8
geneeuchoi <chyj2108@naver.com>
2026-01-28 19:24:30 +0900

    git init

---
+++ src/test/java/com/bank/channelbanking/ChannelBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.channelbanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChannelBankingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
